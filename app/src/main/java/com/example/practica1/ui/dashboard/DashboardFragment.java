7309c41432c9908acab5ff5dee1d7cc497d26fed
DANTE MELO GOMEZ <dante.melo@telcel.com>
2023-05-03 16:13:38 -0600

    Nombre proyecto: practica1. Descripcion: se sube proyecto inicial de practica 1

---
+++ app/src/main/java/com/example/practica1/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,37 @@
+package com.example.practica1.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.practica1.databinding.FragmentLinearBinding;
+
+public class DashboardFragment extends Fragment {
+
+    private FragmentLinearBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        DashboardViewModel dashboardViewModel =
+                new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = FragmentLinearBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+ //       final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
